9f926b293395fa20d4a6d765cc8e71a92f290201
Darr3n <darrensoonyuchien@gmail.com>
2024-09-03 11:43:51 +0800

    Update linedraw.py

--- linedraw.py
+++ linedraw.py
@@ -6,12 +6,8 @@ pygame.display.set_caption("Draw Line Function in Pygame")
 
 run = True
 while run:
-    pygame.time.delay(100)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run=False
-    mouse_pos = pygame.mouse.get_pos()
-    win.fill((0,0,0))
-    pygame.draw.line(win, (0,255,0), (250, 250), mouse_pos, 3)
-    pygame.display.update()
+    for i in range(0, 500, 20):
+        pygame.draw.line(win, (255,255,255), (0, i), (500, i))
+        pygame.draw.line(win, (255,255,255), (i, 0), (i, 500))
+    pygame.display.flip()
 pygame.quit()
