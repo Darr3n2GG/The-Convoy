42e53076169e9a11b88286a9cfc3560ef9d28ca6
Darr3n <darrensoonyuchien@gmail.com>
2024-09-03 14:01:50 +0800

    Update SnakeGame.py

--- SnakeGame.py
+++ SnakeGame.py
@@ -170,10 +170,10 @@ while True:
 
     # Game Over conditions
     # Getting out of bounds
-    if snake_pos[0] < 0 or snake_pos[0] > frame_size_x-10:
-        game_over()
-    if snake_pos[1] < 0 or snake_pos[1] > frame_size_y-10:
-        game_over()
+    # if snake_pos[0] < 0 or snake_pos[0] > frame_size_x-10:
+    #     game_over()
+    # if snake_pos[1] < 0 or snake_pos[1] > frame_size_y-10:
+    #     game_over()
     # Touching the snake body
     for block in snake_body[1:]:
         if snake_pos[0] == block[0] and snake_pos[1] == block[1]:
