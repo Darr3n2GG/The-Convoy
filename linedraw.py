8312bde2c869f828a33f5a4ab9e2f12eb11dfbf3
Darr3n <darrensoonyuchien@gmail.com>
2024-09-03 11:49:49 +0800

    added width of 10 for linedraw

--- linedraw.py
+++ linedraw.py
@@ -7,7 +7,7 @@ pygame.display.set_caption("Draw Line Function in Pygame")
 run = True
 while run:
     for i in range(0, 500, 20):
-        pygame.draw.line(win, (255,255,255), (0, i), (500, i))
-        pygame.draw.line(win, (255,255,255), (i, 0), (i, 500))
+        pygame.draw.line(win, (255,255,255), (0, i), (500, i), 10)
+        pygame.draw.line(win, (255,255,255), (i, 0), (i, 500), 10)
     pygame.display.flip()
 pygame.quit()
