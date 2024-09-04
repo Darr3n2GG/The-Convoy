f3915495c1f35225fdc315d188ae73a0a04d00fa
Cyberredcoat <thatspacenerd498@gmail.com>
2024-09-04 13:54:49 +0800

    Reskin and minor fixes

--- SnakeGame.py
+++ SnakeGame.py
@@ -14,30 +14,12 @@ FRAME_SIZE_Y = 500
 SNAKE_SIZE = 10
 FPS_CONTROLLER = pygame.time.Clock()
 
-
-# Variables
-snake_pos = [FRAME_SIZE_X/2, FRAME_SIZE_Y/2]
-snake_body = [[100, 50], [90, 50], [80, 50]]
-food_pos = [random.randrange(0, FRAME_SIZE_X // SNAKE_SIZE) * SNAKE_SIZE,
-            random.randrange(0, FRAME_SIZE_Y // SNAKE_SIZE) * SNAKE_SIZE]
-food_spawn = True
-direction = 'RIGHT'
-change_to = direction
-score = 0
-speed = 20
-
-landmines = []
-blink_duration = 3000
-fade_duration = 1000
-show_landmines = False
-
+speed = 10
 
 # Sound effect
 pygame.mixer.init(44100, -16, 2, 512)
-background = pygame.mixer.Sound('./soundpack/sonar.mp3')
-detected = pygame.mixer.Sound('./soundpack/enemy_sensed.mp3')
-ended = False
-
+SONAR = pygame.mixer.Sound('./soundpack/sonar.mp3')
+DETECTED = pygame.mixer.Sound('./soundpack/enemy_sensed.mp3')
 
 # Checks for errors encounteREDf
 check_errors = pygame.init()
@@ -51,8 +33,8 @@ else:
 
 
 # Initialise game window
-pygame.display.set_caption('Worm')
-game_window = pygame.display.set_mode((FRAME_SIZE_X, FRAME_SIZE_Y))
+pygame.display.set_caption('The Convoy')
+game_window = pygame.display.set_mode((FRAME_SIZE_X + 1, FRAME_SIZE_Y + 1))
 
 # FPS (frames per second) controller
 fps_controller = pygame.time.Clock()
@@ -63,8 +45,9 @@ def random_pos():
     return [random.randrange(1, (FRAME_SIZE_X//10)) * 10, random.randrange(1, (FRAME_SIZE_Y//10)) * 10]
 
 # Game Over
-def game_over(color, FONT, size):
-    game_over_surface = FONT.render('YOU DIED', True, RED)
+def game_over():
+    font = pygame.font.Font('./font/AtkinsonHyperlegible-Regular.ttf', 100)
+    game_over_surface = font.render('YOU DIED', True, RED)
     game_over_rect = game_over_surface.get_rect()
     game_over_rect.midtop = (FRAME_SIZE_X/2, FRAME_SIZE_Y/4)
     game_window.fill(BLACK)
@@ -76,21 +59,19 @@ def game_over(color, FONT, size):
     sys.exit()
 
 # Score
-# Text aligned to top left of window
 def show_score(color, FONT, size):
-    score_surface = FONT.render('Score : ' + str(score), True, color)
+    score_surface = FONT.render('Checkpoints : ' + str(score), True, color)
     score_rect = score_surface.get_rect()
-    score_rect.topleft = (10, 10)
-    game_window.blit(score_surface, score_rect)
-
+    score_rect.topleft = (10, 15)
     game_window.blit(score_surface, score_rect)
 
 # Speed
 def show_speed(color, FONT, size):
     speed_surface = FONT.render('Speed : ' + str(speed), True, color)
     speed_rect = speed_surface.get_rect()
-    speed_rect.topleft = (10, 40)
+    speed_rect.bottomright = (game_window.get_width() - 10, game_window.get_height() - 10)
     game_window.blit(speed_surface, speed_rect)
+
 
 
 def generate_food_position():
@@ -100,9 +81,20 @@ def generate_food_position():
         if new_food_pos not in snake_body:
             return new_food_pos
 
-def landmine_pos():
-    for i in range(len(landmines)):
-        landmines[i] = random_pos()
+# Variables
+snake_pos = [FRAME_SIZE_X/2, FRAME_SIZE_Y/2]
+snake_body = [[100, 50], [90, 50], [80, 50]]
+food_pos = [random.randrange(0, FRAME_SIZE_X // SNAKE_SIZE) * SNAKE_SIZE,
+            random.randrange(0, FRAME_SIZE_Y // SNAKE_SIZE) * SNAKE_SIZE]
+food_spawn = True
+direction = 'RIGHT'
+change_to = direction
+score = 0
+
+landmines = []
+blink_duration = 2000
+fade_duration = 1000
+show_landmines = False
 
 # Main logic
 while True:
@@ -110,6 +102,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
         # Whenever a key is pressed down
         elif event.type == pygame.KEYDOWN:
             # W -> Up; S -> Down; A -> Left; D -> Right
@@ -144,22 +137,20 @@ while True:
     if change_to == 'RIGHT' and direction != 'LEFT':
         direction = 'RIGHT'
 
+
     # GFX
     game_window.fill(BLACK)
-    for i in range(0, FRAME_SIZE_X, 50):
+
+    # Grid
+    for i in range(0, FRAME_SIZE_X + 1, 50):
         pygame.draw.line(game_window, GREEN, (0, i), (FRAME_SIZE_X, i))
-    for i in range(0, FRAME_SIZE_Y , 50):
+    for i in range(0, FRAME_SIZE_Y + 1, 50):
         pygame.draw.line(game_window, GREEN, (i, 0), (i, FRAME_SIZE_Y))
     for pos in snake_body:
         # Snake body
         # .draw.rect(play_surface, color, xy-coordinate)
         # xy-coordinate -> .Rect(x, y, size_x, size_y)
         pygame.draw.rect(game_window, GREEN, pygame.Rect(pos[0], pos[1], SNAKE_SIZE, SNAKE_SIZE))
-
-    # Sound
-    if not ended:
-        background.play(-1)
-    #detected only plays when enemy is hit
 
     # Snake movements
     if direction == 'UP':
@@ -192,16 +183,20 @@ while True:
 
     # Spawning food on the screen
     if not food_spawn:
-        food_pos = generate_food_position()
+        food_pos = random_pos()
         landmines.insert(0,random_pos()) # Spawn landmine
     food_spawn = True
 
-    pygame.draw.rect(game_window, WHITE, pygame.Rect(food_pos[0], food_pos[1], SNAKE_SIZE, SNAKE_SIZE))
+    # Snake food
+    pygame.draw.rect(game_window, WHITE, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
 
     # Landmine blinking
     current_ticks = pygame.time.get_ticks()
     if not show_landmines and current_ticks - start_ticks >= blink_duration:
         show_landmines = True
+        for i in range(len(landmines)):
+            landmines[i] = random_pos()
+            SONAR.play()
         start_ticks = current_ticks
     elif show_landmines and current_ticks - start_ticks >= fade_duration:
         show_landmines = False
@@ -215,19 +210,24 @@ while True:
     # Touching the snake body
     for block in snake_body[1:]:
         if snake_pos[0] == block[0] and snake_pos[1] == block[1]:
-            game_over(RED, FONT, 100)
+            game_over()
 
-    # landmine
+
+    # Touching landmine
     for landmine_pos in landmines:
         if snake_pos[0] == landmine_pos[0] and snake_pos[1] == landmine_pos[1]:
             snake_body.pop()
             if snake_body == []:
-                game_over(RED, FONT, 100)
+                game_over()
 
+
+    # Show score and speed value
     show_score(WHITE, FONT, 20)
-    show_speed(WHITE, FONT, 20)
+    show_speed(WHITE, FONT, 1)
+    
 
     # Refresh game screen
     pygame.display.update()
+
     # Refresh rate
     FPS_CONTROLLER.tick(speed)
